f922f62d82d01bed9f5ea1c887a5c672c1c4fb2a
Sebastian <65371156+SebRincon@users.noreply.github.com>
2022-06-02 20:06:50 -0500

    Time: 180 ms (15.19%), Space: 9.1 MB (72.45%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.go
+++ 53-maximum-subarray/53-maximum-subarray.go
@@ -3,9 +3,11 @@ func maxSubArray(nums []int) int {
     curSum := 0
     
     for _, num := range nums {
+        // If sum falls to negative reset count
         if curSum < 0{
             curSum = 0
         }
+        // Increment and set maxSum if the current count is larger
         curSum += num
         if curSum > maxSub{
             maxSub = curSum
